913bd3c99045d046e18d8492de5e0df0a8bd0bc3
cidzanella <cidzanella@gmail.com>
2024-01-26 09:16:37 -0300

    added Serilog

--- DymoScaleService.Api/Program.cs
+++ DymoScaleService.Api/Program.cs
@@ -1,11 +1,39 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // hosting windows service 
 builder.Host.UseWindowsService();
 builder.Services.AddWindowsService();
 
+//serilog configuration
+//https://waqasahmeddev.medium.com/structured-logging-with-serilog-in-net-core-6-best-practices-and-setup-99aff5893f33
+//https://www.anmalkov.com/blog/use-serilog-with-minimal-api-or-aspnet-6
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(config).CreateLogger();
+
+// remove default logging providers
+builder.Logging.ClearProviders();
+
+// register Serilog
+builder.Logging.AddSerilog(logger);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/dymoscaleapi/getweight", (ILoggerFactory loggerFactory) =>
+{
+    var logger = loggerFactory.CreateLogger("GetWeight");
+    logger.LogInformation("GetWeight called");
+    return "GetWeight called!";
+});
+
+
+app.MapGet("/dymoscaleapi/hello", () => $"Dymo Scale Service API listening on {config.GetValue<string>("Kestrel:Endpoints:Http:Url")}");
+
+app.Logger.LogInformation("DymoScaleService application started");
 
 app.Run();
