e6611b46c2f90eaa06635587669cc69b6a8376e1
cidzanella <cidzanella@gmail.com>
2024-01-26 06:43:54 -0300

    added nuget packages

--- DymoScaleService.Api/Program.cs
+++ DymoScaleService.Api/Program.cs
@@ -1,4 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// hosting windows service 
+builder.Host.UseWindowsService();
+builder.Services.AddWindowsService();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
